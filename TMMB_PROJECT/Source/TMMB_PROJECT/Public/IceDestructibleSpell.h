b66fcf2fd99a20006aad86890af4a07e3d8de300
Pedro A. Tresgallo <P3dro115@gmail.com>
2019-04-22 20:23:12 -0500

    Initial Push
    Add Ice Destructible Spell. Derived from Base Destructible Spell

---
+++ TMMB_PROJECT/Source/TMMB_PROJECT/Public/IceDestructibleSpell.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseDestructibleSpell.h"
+#include "IceDestructibleSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TMMB_PROJECT_API AIceDestructibleSpell : public ABaseDestructibleSpell
+{
+	GENERATED_BODY()
+	
+};
