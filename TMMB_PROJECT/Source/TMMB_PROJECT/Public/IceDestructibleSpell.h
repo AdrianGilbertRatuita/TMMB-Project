0b4992c9d67ecae2f888ad1dfecc165392d358d4
Pedro A. Tresgallo <P3dro115@gmail.com>
2019-04-25 15:31:51 -0500

    Revert "Add Small Changes"
    This reverts commit 5326733ce766e0ae764ec9853716a5df03b5406d.

--- TMMB_PROJECT/Source/TMMB_PROJECT/Public/IceDestructibleSpell.h
+++ TMMB_PROJECT/Source/TMMB_PROJECT/Public/IceDestructibleSpell.h
@@ -2,11 +2,9 @@
 
 #pragma once
 
-#include "Engine.h"
 #include "CoreMinimal.h"
 #include "BaseDestructibleSpell.h"
 #include "IceDestructibleSpell.generated.h"
-#include "Components/PrimitiveComponent.h"
 
 /**
  * 
@@ -15,14 +13,5 @@ UCLASS()
 class TMMB_PROJECT_API AIceDestructibleSpell : public ABaseDestructibleSpell
 {
 	GENERATED_BODY()
-
-
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-
-	UFUNCTION()
-	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 	
 };
