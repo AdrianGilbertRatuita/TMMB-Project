72764986c17a566292624f4614190b693728f216
Pedro A. Tresgallo <P3dro115@gmail.com>
2019-04-25 15:43:05 -0500

    Fix Previous Push
    Had accidentally deleted the uproject file

--- TMMB_PROJECT/Source/TMMB_PROJECT/Private/IceDestructibleSpell.cpp
+++ TMMB_PROJECT/Source/TMMB_PROJECT/Private/IceDestructibleSpell.cpp
@@ -2,3 +2,22 @@
 
 #include "IceDestructibleSpell.h"
 
+void AIceDestructibleSpell::Tick(float DeltaTime)
+{
+	// Do stuff
+	// This is basically Update() {	};
+}
+
+void AIceDestructibleSpell::OnOverlapBegin(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	// This is basically onTriggerEnter() {	};
+	if (OtherActor->ActorHasTag("Player"))
+	{
+		// Do stuff bitch
+		FTransform X = GetActorTransform();
+		//FMath::
+		//GetWorld()->GetDeltaSeconds();
+		FActorSpawnParameters Y;
+		//GetWorld()->SpawnActor(/*spawn the blueprint*/)
+	}
+}
